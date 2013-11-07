7ef81db3ef6d24e233b1f68e07910ad253057550
Theasker <theasker@gmail.com>
2013-11-07 08:51:10 +0100

    Commit inicial tarea2

---
+++ tarea2.php
@@ -0,0 +1,32 @@
+<?php
+//Defino dos variables con mi nombre y apellidos
+
+$nom = "Mauricio";
+$ape = "Segura Ariño";
+
+
+//Visualizo el texto con echo y print, por ejemplo en mi caso (deben de aparecer las comillas del ejemplo
+// mi nombre es "manolo" y mi apellido es "romero"
+//1)con echo pasando varios argumentos (separadados por coma)
+echo("");
+
+//2)con print
+
+
+//Explica en el fichero diferencias entre echo y print y semejanzas.
+//Por qué puedo pasar los argumentos sin usar paréntesis
+//Puedo hacerlo con printf
+/*Sintaxis heredoc,*/
+//Asigna a una variable llamada informe un texto de cinco líneas,
+//la etiqueta de finalización es FIN
+//Posteriormente visualizas el texto
+// El contenido de 'informe' es
+// aquí aparecer el contenido del infoorme
+// respetando el número de 5 líneas asignadas previamente";
+//Tener cuidado con que la etiqueta no lleve en esa línea ningún otro carácter (espacios en blanco o tabulacones)
+//Probando variables
+//Crea una variable y asígnale un valor
+//visualiza el valor de la variable y el tipo que eś
+//Cambia a los siguientes tipos (boolean, float, string y null y visualizar su valor)
+//Prueba a ver el valor y tipo de una variable no definida previamente
+?>
